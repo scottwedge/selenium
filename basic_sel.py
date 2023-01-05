85f213889106213ab28c34bdeeb32b815e4dabd6
Scott Wedge <scott.wedge@gmail.com>
2023-01-04 20:38:01 -0500

    Successful first get of web page (thanks to stackoverflow)

--- basic_sel.py
+++ basic_sel.py
@@ -2,5 +2,12 @@
 
 # Imports
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+# according to https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python
 
+
+#
+driver =  webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
 
