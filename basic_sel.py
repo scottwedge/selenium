b9800d3aa084f8223705f1e1246b52010500b13b
Scott Wedge <scott.wedge@gmail.com>
2023-01-05 22:18:41 -0500

    Access second site. Had to set repeat 'driver' setting else fails

--- basic_sel.py
+++ basic_sel.py
@@ -2,13 +2,37 @@
 
 # Imports
 from selenium import webdriver
+# according to https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-# according to https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python
 
+# from https://www.youtube.com/watch?v=kpONBQ3muLg
+#from selenium.webdriver.firefox.service import Service
+#from selenium.webdriver.firefox.options import Options
 
-#
+# launch Chrome browser
 driver =  webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.google.com")
 
 print(driver.title)  # Get and print window title
+driver.close()
+
+
+# select second web site
+driver =  webdriver.Chrome(service=Service(ChromeDriverManager().install()))  # Must repeat driver setting else fails here
+driver.get("https://techwithtim.net")
+print(driver.title)
+driver.quit()
+
+
+# configureFirefox web browser
+#user_agent = 
+#firefox_driver = 
+#firefox_service = 
+#firefox_options =
+
+# launch Firefox web browser
+#driver = webdriver.Firefox()
+#driver.get("https://techwithtim.net")
+#print(driver.title)
+#driver.close()
