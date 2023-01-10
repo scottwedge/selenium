d138d7a64a3ac8b562a5a1f568719952d3cc261b
Scott Wedge <scott.wedge@gmail.com>
2023-01-09 21:54:05 -0500

    Remove firefox browser, NEW: use demo.seleniumeasy.com website, send for element by id, enter text into element using '.send_keys', print current time using time.asctime()

--- basic_sel.py
+++ basic_sel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # Imports
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -31,18 +32,21 @@ print(driver.current_url)  # Current URL
 search_string = "Tutorials"  # Define string to search for
 src = driver.page_source  # Capture all page source
 print("Search string found in page source = ",search_string in src)  # Booleans result if search string found in page
-
 driver.quit()
 
 
-# configureFirefox web browser
-#user_agent = 
-#firefox_driver = 
-#firefox_service = 
-#firefox_options =
+# Use the demo.seleniumeasy.com site with all of its practice pages
+# 1. Input forms - Simple Demo form at basic-first-form-demo.html
+# NEW: search for element with "id" using "By.ID"
+# NEW: print current time using "time.asctime()"
+# NEW: enter text into found text box element using text_box_element.send_keys("text-goes-here")
 
-# launch Firefox web browser
-#driver = webdriver.Firefox()
-#driver.get("https://techwithtim.net")
-#print(driver.title)
-#driver.close()
+driver =  webdriver.Chrome(service=Service(ChromeDriverManager().install()))  
+driver.get("http://demo.seleniumeasy.com/basic-first-form-demo.html")
+
+# Search for id="user-message"
+element_by_id = driver.find_element(By.ID, "user-message")
+print("Found element for 'id='user-message''=", element_by_id)
+element_by_id.send_keys("Hello there. The time is: ", time.asctime())
+time.sleep(10)  # Pause so can examine if textbox has entered values
+
