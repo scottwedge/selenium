a92d00bfa3ab55cf279e7283012a2ac0e7f5a88a
Scott Wedge <scott.wedge@gmail.com>
2023-01-11 21:40:09 -0500

    NEW: Select button element using Xpath and press button with 'click()'

---
+++ xpath1_sel.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Imports
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# Use the demo.seleniumeasy.com site with all of its practice pages
+# 1. Input forms - Simple Demo form at basic-first-form-demo.html
+
+driver =  webdriver.Chrome(service=Service(ChromeDriverManager().install()))  
+driver.get("http://demo.seleniumeasy.com/basic-first-form-demo.html")
+
+# Search for id="user-message"
+element_by_id = driver.find_element(By.ID, "user-message")
+#print("Found element for 'id='user-message''=", element_by_id)
+
+# Enter text into the element using ".send_keys" 
+element_by_id.send_keys("Hello there. The time is: ", time.asctime())
+time.sleep(10)  # Pause so can examine if textbox has entered values
+
+
+# Need to select button with text label of "Show Message"
+# NEW: Use relative xpath and search by Xpath
+# NEW: ".click()" to press the button element
+
+xpath_1 = '//button[@class="btn btn-default"]'  # This selects the first of two buttons
+xpath_2 = '//*[@type="button" and @onclick="showInput();"]'  # This only selects the first button
+
+show_message_button = driver.find_element(By.XPATH, xpath_2)
+show_message_button.click()  # Then click on the button
+time.sleep(10)  # Pause so can examine if click has caused value to be displayed
+
