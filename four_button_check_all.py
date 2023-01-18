4075323b0a043a7f4fc060f956af0e1f71f4150b
Scott Wedge <scott.wedge@gmail.com>
2023-01-17 20:58:19 -0500

    Check that button name changes and all boxes are checked. Then uncheck each box.

--- four_button_check_all.py
+++ four_button_check_all.py
@@ -60,30 +60,32 @@ else:
 
 time.sleep(10)
 
-
-
-
-
-
-# Click on checkbox
+# Click on "check All" button and verify that:
+# 1. the button name changes and 
+# 2. all four check boxes are now checked
 button_id.click()
 
-# Select the "Check All" button and verify that all four checkboxes are checked
 
-# Verify that the button name has changed to "Check All"
-#print(dir(button_id))  #DEBUG
-print(button_id.get_attribute("value"))  # This value should be "Uncheck All"
+# Verify that the button name has changed to "Uncheck All"
+button_name = button_id.get_attribute("value")  # This value should be "Uncheck All"
+if button_name == "Uncheck All":
+    print("The button name has correctly changed to:", button_name)
+else:
+    print("The button name is now:", button_name)
+ 
+# Check that all four checkboxes are now selected
 
-checkbox_1 = driver.find_element(By.XPATH, box_1)
+if checkbox_1.is_selected() and checkbox_2.is_selected() and checkbox_3.is_selected() and checkbox_4.is_selected():
+    print("All four checkboxes are selected")
+
+# Click on each of four checkboxes to uncheck it
 checkbox_1.click()
 time.sleep(1)
-checkbox_2 = driver.find_element(By.XPATH, box_2)
 checkbox_2.click()
 time.sleep(1)
-checkbox_3 = driver.find_element(By.XPATH, box_3)
 checkbox_3.click()
 time.sleep(1)
-checkbox_4 = driver.find_element(By.XPATH, box_4)
 checkbox_4.click()
+time.sleep(1)
 
 time.sleep(10)
