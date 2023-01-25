4fe900b21c08ffb5214f9a216334768b63737118
Scott Wedge <scott.wedge@gmail.com>
2023-01-24 21:55:24 -0500

    This unit test does not work yet

--- unit_testing.py
+++ unit_testing.py
@@ -2,27 +2,14 @@
 
 # Imports
 import unittest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 # Functions
-#def single_input_field(msg):
-#    id = "user-message"
-#    enter_message_box = driver.find_element(By.ID, id)
+def return_one():
+    return 1
 
-class TestWebPage(unittest.TestCase):
-    def test_single_input_field(self):
-        self.assertTrue(True)
 
-# launch Chrome browser
-driver =  webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def test_return_one():
+    assert return_one() == 1
 
-web_site = "https://demo.seleniumeasy.com/basic-first-form-demo.html"
-driver.get(web_site)
-
-print("unit testing")
 
